34cd26f0a8edca0764bb1c78e00fb2e00a22d850
my-plex <paul.loban@gmail.com>
2019-11-22 14:32:06 +0200

    Phea
    new project

--- constant.php
+++
@@ -1,7 +0,0 @@
-<?php
-//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-define('SITE_KEY',"SITE_KEY"); 
-define('SECRET_KEY',"SECRET_KEY");
-define('MAIL_FROM', 'info@ipcloudlp.com');
-define('MAIL_TO', 'info@ipcloudlp.com');
-
